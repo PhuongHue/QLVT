ba484916bfb5d161953575ae53ee137a786cbaf4
levanthanh-ptit <levanthanh.ptit@gmail.com>
2019-07-09 14:43:33 +0700

    update

--- QLVT/Properties/Settings.Designer.cs
+++ QLVT/Properties/Settings.Designer.cs
@@ -36,8 +36,8 @@ namespace QLVT.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-17735P0\\PH_02;Initial Catalog=QLVT_DATHANG;Persist Security I" +
-            "nfo=True;User ID=sa;Password=123")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-33BKUFB\\SV_1;Initial Catalog=QLVT_DATHANG;Persist Security In" +
+            "fo=True;User ID=sa;Password=123")]
         public string QLVT_DATHANG_CN_ConnectionString {
             get {
                 return ((string)(this["QLVT_DATHANG_CN_ConnectionString"]));
